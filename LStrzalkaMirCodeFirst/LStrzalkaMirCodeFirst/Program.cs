e7d0181794c04652be28e08226d247355ed3e2df
Łukasz Strzałka <lusr@ailleron.com>
2018-05-30 23:53:25 +0200

    first commit

---
+++ LStrzalkaMirCodeFirst/LStrzalkaMirCodeFirst/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace LStrzalkaMirCodeFirst
+{
+    public class SimpleTabCF
+    {
+        public int id { get; set; }
+        public int val { get; set; } 
+    }
+    public class CodeFirstDBLStrzalka : DbContext
+    {
+        public DbSet<SimpleTabCF> SimpleTabCFs { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new CodeFirstDBLStrzalka()) 
+            { 
+                while(true)
+                {
+                    Console.Write("Enter a val or type exit: ");
+                    var val = Console.ReadLine(); 
+                    if(val == "exit"){
+                        break;
+                    }
+ 
+                    var blog = new SimpleTabCF { val = Int32.Parse(val) }; 
+                    db.SimpleTabCFs.Add(blog); 
+                    db.SaveChanges(); 
+ 
+                    // Display all Blogs from the database 
+                    var query = from b in db.SimpleTabCFs 
+                                orderby b.val 
+                                select b; 
+ 
+                    Console.WriteLine("All vals in the database:"); 
+                    foreach (var item in query) 
+                    { 
+                        Console.WriteLine(item.val); 
+                    }
+                }
+            } 
+        }
+    }
+}
---
+++ LStrzalkaMirDBFirst/LStrzalkaMirDBFirst/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LStrzalkaMirDBFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SimpleDBLStrzalka() ) 
+                {
+
+                    while (true)
+                    {
+                        Console.Write("Insert value or type exit to leave: ");
+                        var exit = Console.ReadLine();
+                        if (exit == "exit")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            var blog = new simpleTab { val = Int32.Parse(exit) };
+                            db.simpleTab.Add(blog);
+                            db.SaveChanges();
+                            var query = from b in db.simpleTab
+                                        orderby b.id
+                                        select b;
+
+                            Console.WriteLine("All vals in the database:");
+                            foreach (var item in query)
+                            {
+                                Console.WriteLine(item.val);
+                            }
+                        }
+                    }
+            } 
+        }
+    }
+}
